7a66863cc6e81bc8298d7a1f109aebb2bc283ae5
Giga <gabrielgiganti@hotmail.com>
2020-11-05 19:26:47 -0300

    novo folderr

---
+++ Aula5.kt
@@ -0,0 +1,66 @@
+package com.example.exercicioskotlin
+
+package com.example.exerciciosoutubronovembro
+
+fun main() {
+    val primeiroNumero = 1
+    val segundoNumero = 3
+    val terceiroNumero = 5
+
+    val texto1 = "meu nome"
+    val texto2 = "não é meu nome"
+
+    val numeroInteiro = 2
+
+    var numeroContagem = 0
+
+    var numA = 1
+    var numB = 2
+    var numC = 3
+    var numD = 4
+
+// exercicio 1
+//    if (primeiroNumero > segundoNumero && primeiroNumero > terceiroNumero) {
+//            println("o maior número é o $primeiroNumero")
+//    } else if (segundoNumero > terceiroNumero && segundoNumero > primeiroNumero) {
+//            println("o maior número é o $segundoNumero")
+//    }
+//    else {
+//         println("o maior número é o $terceiroNumero")
+//    }
+
+// exercicio 2
+//    if (texto1 === texto2) {
+//        println(false)
+//    } else {
+//        println(true)
+//    }
+
+// exercicio 3
+//    when {
+//        numeroInteiro % 2 == 0 -> println(true)
+//        else -> println(false)
+//    }
+
+// exercicio 4
+//    for (numeroContagem in 1..100 step 2)
+//    println(numeroContagem)
+// }
+
+    // exercicio 5
+
+//   fun exercicio5(numA : Int, numB : Int, numC : Int, numD : Int) : Boolean {
+//       if (numA > numC && numA > numD) {
+//           return true
+//       } else if (numB > numC && numB > numD) {
+//           return true
+//       } else {
+//           return false
+//       }
+//       val resultado = (numA, numB, numC, numD)
+//       println(resultado)
+//   }
+
+
+
+}
